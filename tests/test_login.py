dc3645f6f39a1f74b54bec34932b0d625afd4173
rubeldm2010 <rubeldm2010@gmail.com>
2025-05-19 12:16:27 -0500

    addddd

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,6 +5,6 @@ def test_first_example(page: Page):
     page.goto("https://abc12309.sg-host.com/")
     title=page.title()
     print("--------------------------->", title)
-    print("--------------------------->", title2)
+    print("--------------------------->"+ "title2")
     assert "Iqrashoppingdemo" in title
     page.wait_for_timeout(2000)
